d5c38792c547d775c9fd1f508aaebf69b1d8dc84
victordevera24 <victordevera24@gmail.com>
2022-05-16 13:43:54 -0700

    adding model

--- collect_project/main_app/views.py
+++ collect_project/main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Fish
 
 # Add the following import
 from django.http import HttpResponse
@@ -6,17 +7,6 @@ from django.http import HttpResponse
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-class Fish:
-    def __init__(self, name, size, description):
-        self.name = name
-        self.size = size
-        self.description = description
-
-fish = [
-    Fish('Exclamation Point Rasbora', '1 inch', 'Exclamation Point positioned horizontally down side of fish'),
-    Fish('Celestial Pearl Danio', '1.5 inches', 'Dark blue body with small random white spots covering body'),
-    Fish('Otocinclus', '2 inches', 'Eats organisms off surfaces using sucker mouth.')
-]
 
 # Define the home view
 def home(request):
@@ -39,4 +29,5 @@ def fish_index(request):
     http://localhost:8000/fish/
     """
     logging.info('calling fish_index')
+    fish = Fish.objects.all()
     return render(request, 'fish/index.html', {'fish' : fish})
